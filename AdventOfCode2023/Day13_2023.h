6ecdcb9e37e789d3a44218e80147af470115014c
Chris Smith <casmith7901@gmail.com>
2023-12-13 13:15:24 +0000

    day13 solutions

--- AdventOfCode2023/Day13_2023.h
+++ AdventOfCode2023/Day13_2023.h
@@ -3,10 +3,186 @@
 #include <iostream>
 #include "input_day13_2023.h"
 #include "Util.h"
+#include <cassert>
 
 class Day13_2023
 {
 public:
     Day13_2023() = default;
     ~Day13_2023() = default;
+
+    std::vector<std::vector<std::string>> parseInputIntoPuzzles(std::vector<std::string> puzzleInput)
+    {
+        std::vector<std::vector<std::string>> puzzles;
+        std::vector<std::string> puzzle;
+        for (std::string line : puzzleInput)
+        {
+            if (line.empty())
+            {
+                puzzles.push_back(puzzle);
+                puzzle.clear();
+            }
+            else
+            {
+                puzzle.push_back(line);
+            }
+        }
+
+        return puzzles;
+    }
+
+    std::vector<std::vector<int>> parsePuzzle(std::vector<std::string> puzzleInput)
+    {
+        std::vector<std::vector<int>> matrix;
+        for (std::string s : puzzleInput)
+        {
+            matrix.push_back(std::vector<int>());
+            for (char c : s)
+            {
+                if (c == '#')
+                {
+                    matrix[matrix.size() - 1].push_back(1);
+                }
+                else
+                {
+                    matrix[matrix.size() - 1].push_back(0);
+                }
+            }
+        }
+
+        return matrix;
+    }
+
+    bool isSymmetricalRow(int row, std::vector<std::vector<int>> matrix, int numMistakes)
+    {
+        int min = 0;
+        int max = matrix.size();
+        getMinMax(row, min, max);
+        if (max <= min)
+        {
+            return false;
+        }
+        for (int i = row; i >= min; --i)
+        {
+            int row1 = i;
+            int row2 = row + (row - i) + 1;
+            
+            for (int j = 0; j < matrix[row1].size(); ++j)
+            {
+                if (matrix[row1][j] != matrix[row2][j])
+                {
+                    if (numMistakes > 0)
+                    {
+                        --numMistakes;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return numMistakes == 0;
+    }
+
+    void getMinMax(int col, int& min, int& max)
+    {
+        int stepsDown = col - min + 1;
+        int stepsUp = max - (col + 1);
+        int steps = std::min(stepsUp, stepsDown);
+        min = col - steps + 1;
+        max = col + steps + 1;
+    }
+
+    bool isSymmetricalCol(int col, std::vector<std::vector<int>> matrix, int numMistakes)
+    {
+        int min = 0;
+        int max = matrix[0].size();
+        getMinMax(col, min, max);
+        if (max <= min)
+        {
+            return false;
+        }
+        for (int i = col; i >= min; --i)
+        {
+            int col1 = i;
+            int col2 = col + (col - i) + 1;
+
+            for (int j = 0; j < matrix.size(); ++j)
+            {
+                if (matrix[j][col1] != matrix[j][col2])
+                {
+                    if (numMistakes > 0)
+                    {
+                        --numMistakes;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return numMistakes == 0;
+    }
+
+
+    int findSymmetryPoint(std::vector<std::vector<int>> matrix, bool secondPart)
+    {
+        int numMistakes = 0;
+        if (secondPart)
+        {
+            numMistakes = 1;
+        }
+
+        bool found = false;
+        int retVal = 0;
+        for (int i = 0; i < matrix[0].size(); ++i)
+        {
+            if (isSymmetricalCol(i, matrix, numMistakes))
+            {
+                if (!found)
+                {
+                    retVal = (i + 1);
+                    found = true;
+                }
+                else
+                {
+                    assert(false);
+                }
+            }
+        }
+        for (int i = 0; i < matrix.size(); ++i)
+        {
+            if (isSymmetricalRow(i, matrix, numMistakes))
+            {
+                if (!found)
+                {
+                    retVal = 100 * (i + 1);
+                    found = true;
+                }
+                else
+                {
+                    assert(false);
+                }
+            }
+        }
+        assert(found);
+        return retVal;
+    }
+
+    int findTotal(std::vector<std::string> input, bool secondPart)
+    {
+        int total = 0;
+        std::vector<std::vector<std::string>> puzzleInputs = parseInputIntoPuzzles(input);
+        for (std::vector<std::string> puzzle : puzzleInputs)
+        {
+            std::vector<std::vector<int>> pz = parsePuzzle(puzzle);
+            total = total + findSymmetryPoint(pz, secondPart);
+        }
+
+        return total;
+    }
 };
--- UnitTests/test_day13_2023.cpp
+++ UnitTests/test_day13_2023.cpp
@@ -2,7 +2,91 @@
 #include "Day13_2023.h"
 #include "input_day13_2023.h"
 
+TEST(TestDay13_2023, TestExample) {
+    Day13_2023 day13;
+    std::vector<std::string> puzzleInput = {
+        "#.##..##.",
+        "..#.##.#.",
+        "##......#",
+        "##......#",
+        "..#.##.#.",
+        "..##..##.",
+        "#.#.##.#.",
+        "",
+        "#...##..#",
+        "#....#..#",
+        "..##..###",
+        "#####.##.",
+        "#####.##.",
+        "..##..###",
+        "#....#..#",
+        ""
+    };
+
+    std::vector<std::vector<std::string>> puzzles = day13.parseInputIntoPuzzles(puzzleInput);
+    std::vector<std::vector<int>> p1 = day13.parsePuzzle(puzzles[0]);
+    std::vector<std::vector<int>> p2 = day13.parsePuzzle(puzzles[1]);
+    EXPECT_EQ(day13.isSymmetricalRow(1, p1, 0), false);
+    EXPECT_EQ(day13.isSymmetricalRow(1, p2, 0), false);
+    EXPECT_EQ(day13.isSymmetricalRow(3, p2, 0), true);
+    EXPECT_EQ(day13.isSymmetricalRow(4, p2, 0), false);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 5);
+    EXPECT_EQ(day13.findSymmetryPoint(p2, false), 400);
+    EXPECT_EQ(day13.findTotal(puzzleInput, false), 405);
+    EXPECT_EQ(day13.findTotal(puzzleInput, true), 400);
+}
+
+TEST(TestDay13_2023, Test1CaseByCase)
+{
+    Day13_2023 day13;
+    std::vector<std::vector<std::string>> puzzles = day13.parseInputIntoPuzzles(Input2023::day13_input);
+    std::vector<std::vector<int>> p1 = day13.parsePuzzle(puzzles[0]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 400);
+    p1 = day13.parsePuzzle(puzzles[1]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 2);
+    p1 = day13.parsePuzzle(puzzles[2]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 4);
+    p1 = day13.parsePuzzle(puzzles[3]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 200);
+    p1 = day13.parsePuzzle(puzzles[4]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 1);
+    p1 = day13.parsePuzzle(puzzles[5]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 800);
+    p1 = day13.parsePuzzle(puzzles[6]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 100);
+    p1 = day13.parsePuzzle(puzzles[7]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 1);
+    p1 = day13.parsePuzzle(puzzles[8]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 9);
+    p1 = day13.parsePuzzle(puzzles[9]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 1400);
+    p1 = day13.parsePuzzle(puzzles[10]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 12);
+    p1 = day13.parsePuzzle(puzzles[11]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 2);
+    p1 = day13.parsePuzzle(puzzles[12]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 11);
+    p1 = day13.parsePuzzle(puzzles[13]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 3);
+    p1 = day13.parsePuzzle(puzzles[14]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 6);
+    p1 = day13.parsePuzzle(puzzles[15]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 900);
+    p1 = day13.parsePuzzle(puzzles[16]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 13);
+    p1 = day13.parsePuzzle(puzzles[17]);
+    EXPECT_EQ(day13.findSymmetryPoint(p1, false), 11);
+}
+
+
 TEST(TestDay13_2023, Test1) {
-	Day13_2023 day13;
-	EXPECT_EQ(true, true);
+    Day13_2023 day13;
+    // 31940 too high
+    // 26891 too low
+    EXPECT_EQ(day13.findTotal(Input2023::day13_input, false), 30802);
+}
+
+TEST(TestDay13_2023, Test2) {
+    Day13_2023 day13;
+    EXPECT_EQ(day13.findTotal(Input2023::day13_input, true), 37876);
 }
